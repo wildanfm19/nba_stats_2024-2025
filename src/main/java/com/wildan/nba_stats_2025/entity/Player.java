79e8e7058aab32b7a61d949e3006d6ae40dc3219
wildanfm19 <dracko.maaruftm@gmail.com>
2025-10-29 23:37:14 +0700

    feat : Connect to postgresql and make a table name player_stats

--- src/main/java/com/wildan/nba_stats_2025/entity/Player.java
+++ src/main/java/com/wildan/nba_stats_2025/entity/Player.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(name = "player_stats")
 public class Player {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
