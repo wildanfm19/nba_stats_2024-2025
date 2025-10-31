7be1f291faec5ab8cfc2efb82475997008285da3
wildanfm19 <dracko.maaruftm@gmail.com>
2025-10-31 15:43:45 +0700

    Added CORS on PlayerController

--- src/main/java/com/wildan/nba_stats_2025/controller/PlayerController.java
+++ src/main/java/com/wildan/nba_stats_2025/controller/PlayerController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/player/stats")
+@CrossOrigin("*")
 public class PlayerController {
 
     private final PlayerService playerService;
